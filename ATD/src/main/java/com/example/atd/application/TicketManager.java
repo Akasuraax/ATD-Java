178e37a52d9d59c3474db26a5e09c988dfb6026d
Arthur <arthur.morelon@gmail.com>
2024-04-23 22:10:54 +0200

    add menu déroulant

--- ATD/src/main/java/com/example/atd/application/TicketManager.java
+++ ATD/src/main/java/com/example/atd/application/TicketManager.java
@@ -5,7 +5,9 @@ import com.example.atd.model.Ticket;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -36,6 +38,29 @@ public class TicketManager {
         ListView<Ticket> assignedListView = new ListView<>();
         assignedListView.setItems(assignedTickets);
         assignedListView.setCellFactory(param -> new TicketCell());
+
+        // Création du ContextMenu
+        ContextMenu contextMenu = new ContextMenu();
+
+        // Création des MenuItem et ajout au ContextMenu
+        MenuItem menuItem1 = new MenuItem("Option 1");
+        MenuItem menuItem2 = new MenuItem("Option 2");
+        contextMenu.getItems().addAll(menuItem1, menuItem2);
+
+        // Association du ContextMenu aux ListView
+        unassignedListView.setContextMenu(contextMenu);
+        assignedListView.setContextMenu(contextMenu);
+
+        // Ajout des écouteurs d'événements aux MenuItem si nécessaire
+        menuItem1.setOnAction(event -> {
+            // Code à exécuter lorsque l'utilisateur sélectionne "Option 1"
+            System.out.println("Option 1 sélectionnée");
+        });
+
+        menuItem2.setOnAction(event -> {
+            // Code à exécuter lorsque l'utilisateur sélectionne "Option 2"
+            System.out.println("Option 2 sélectionnée");
+        });
 
         HBox root = new HBox(unassignedListView, assignedListView);
         Scene scene = new Scene(root, 800, 600);
